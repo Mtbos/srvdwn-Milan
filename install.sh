899783df117f6234b70f5e984d58c645912e1dcc
Mtbos <113591367+Mtbos@users.noreply.github.com>
2022-09-16 10:31:19 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+This tool is install as follow:
+first copy the git code from here
+git clone the git code of my tool in the linux terminal
+Than provide it the excute command
+than type python3 srvdwn-Milan to the the file
+that its! Enjoy the tool
+
